11b02cb783151f065575231e4aa6493ea0213c23
XanderJ1 <bzakariyya6@gmail.com>
2025-10-10 09:57:08 +0000

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    ssr: true,
+    nitro: {
+        preset: 'vercel',
+    },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css','primeicons/primeicons.css', '@fontsource/atkinson-hyperlegible/700.css', "@fontsource-variable/inter"],
